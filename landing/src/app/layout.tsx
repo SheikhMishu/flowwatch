320b43b486aefb5ebea9630a2f0a1e6dceba32e5
Sheikh Mishu <sheikh.mohammad.mishu@gmail.com>
2026-05-04 20:07:27 +1000

    Update GA4 measurement ID to G-LGJ8543ZP2

--- landing/src/app/layout.tsx
+++ landing/src/app/layout.tsx
@@ -68,8 +68,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {children}
         <Script id="fb-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','942346332108194');fbq('track','PageView');` }} />
         <Script id="ms-clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","wkmrzvenzm");` }} />
-        <Script id="ga4-loader" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-MXFDSFWL1X" />
-        <Script id="ga4-config" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-MXFDSFWL1X');` }} />
+        <Script id="ga4-loader" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-LGJ8543ZP2" />
+        <Script id="ga4-config" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-LGJ8543ZP2');` }} />
         <Script
           defer
           data-domain="flowmonix.com"
